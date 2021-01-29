d9b4ccd1dd01cb7d08e070e7d35713f9cec8fc39
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-01-29 15:40:53 -0500

    menu styling

--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -4,13 +4,16 @@ import "./menu-item.styles.scss"
 const MenuItem = ({title, imageUrl, size}) => {
 
      return(
-          <div style={{backgroundImage: `url(${imageUrl})`}}
+          <div 
           className={`${size} menu-item`}>
+          <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}>
           <div className="content">
                <h1 className="title">{title}</h1>
                <span className="subtitle">Shop Now</span>
           </div>
      </div>
+     </div>
+     
 
      )
 
