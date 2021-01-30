59f38676aacb080a34c6e0e7a50968f88da65e10
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-01-30 16:14:18 -0500

    add collection and shop compon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Homepage from "./pages/homepage/Homepage.js"
 import "./components/menu-item/MenuItem"
-
+import { Switch, Route } from 'react-router-dom'
+import Shop from './pages/shop/Shop.js'
 
 
 
@@ -10,8 +11,14 @@ import "./components/menu-item/MenuItem"
 function App() {
   return (
     <div>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route path='/shop' component={Shop}/>
       <Homepage/>
+      </Switch> 
     </div>
   );
 }
+
+
 export default App;
---
+++ src/pages/shop/Shop.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import SHOP_DATA from './shop.data'
+import CollectionPreview from '../../components/collection-preview/CollectionPreview.js'
+
+class Shop extends React.Component {
+constructor(){
+     super()
+
+
+     this.state= {
+          collections: SHOP_DATA
+     }
+
+}
+
+render() {
+     const {collections} = this.state;
+     return (
+     <div className="shop-page">
+          {
+               collections.map(({id, ...otherCollectionProps}) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+     ))
+          }
+
+     </div>
+     )
+     
+
+}
+
+}
+
+export default Shop
