c93782ac0f94f966f89a38872271295b6580e987
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-02-01 17:41:27 -0500

    Collection Item comp and styling

--- src/components/collection-preview/CollectionPreview.js
+++ src/components/collection-preview/CollectionPreview.js
@@ -1,6 +1,6 @@
-import { findAllByTitle } from '@testing-library/react';
 import React from 'react';
 import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/CollectionItem.js"
 
 const CollectionPreview = ({title, items}) => {
 
@@ -8,14 +8,14 @@ const CollectionPreview = ({title, items}) => {
           <div className="collection-preview">
                <h1 className="title">{title.toUpperCase()}</h1>
                <div className="preview">
-                    {items.filter((item, idx) => idx < 4).map (item => (
-                         <div key={item.id}>{item.name}</div>
+                    {items.filter((item, idx) => idx < 4).map (({id, ...otherItemProps}) => (
+                         <CollectionItem key={id} {...otherItemProps}/>
                     ))}
                </div>
           </div>
      )
 
 
-}
+} 
 
 export default CollectionPreview
