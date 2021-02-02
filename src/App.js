4fe24d5e779e52c4c97a75d2c3f625b4aef71820
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-02-02 17:32:52 -0500

    Header component, styling, routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import './App.css';
-import Homepage from "./pages/homepage/Homepage.js"
-import "./components/menu-item/MenuItem"
-import { Switch, Route } from 'react-router-dom'
-import Shop from './pages/shop/Shop.js'
-
-
-
-
+import "./App.css";
+import Homepage from "./pages/homepage/Homepage.js";
+import "./components/menu-item/MenuItem";
+import { Switch, Route } from "react-router-dom";
+import Shop from "./pages/shop/Shop.js";
+import Header from "./components/header/Header.js";
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
-        <Route exact path='/' component={Homepage}/>
-        <Route path='/shop' component={Shop}/>
-      <Homepage/>
-      </Switch> 
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={Shop} />
+        <Homepage />
+      </Switch>
     </div>
   );
 }
-
 
 export default App;
