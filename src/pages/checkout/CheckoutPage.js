e4eee2b9d388d72dc143475a2ddebcbca812d755
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-02-15 15:31:50 -0500

    Update cart to use selectors and checkout item linked to checkoutPage comp

--- src/pages/checkout/CheckoutPage.js
+++ src/pages/checkout/CheckoutPage.js
@@ -6,6 +6,7 @@ import {
   selectCartItems,
   selectCartTotal,
 } from "../../redux/cart/cartSelectors.js";
+import CheckoutItem from "../../components/checkout-item/CheckoutItem.js";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -26,7 +27,9 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map((cartItem) => cartItem.name)}
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
     <div className="total">
       <span>TOTAL:${total}</span>
     </div>
