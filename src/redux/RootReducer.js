140ab3cf4e2dad61c55a4a1d9b3dc6591be95272
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-02-15 17:12:33 -0500

    Refactor to add directory comp to redux

--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -4,6 +4,7 @@ import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/userReducer.js";
 import cartReducer from "./cart/cartReducer.js";
+import directoryReducer from "./directory/directoryReducer.js"
 
 const persistConfig = {
   key: "root",
