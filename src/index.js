7d311534290d0e8211fd72dedf88932a37ac87f7
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-02-15 16:52:18 -0500

    Add Redux persist to save cart items on refresh

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter} from 'react-router-dom'
-import { Provider } from 'react-redux';
-import store from "./redux/store.js"
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-<Provider store={store}>
-<BrowserRouter>
-    <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
-    </Provider>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
 
 reportWebVitals();
