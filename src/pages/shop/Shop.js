ce17ad6ab28d6904cc1a8353a7d88cd748fa1cba
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-02-16 14:23:55 -0500

    Add Collections Overview component and move selectors and connect from shop page comp into it

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,29 +1,16 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import CollectionPreview from '../../components/collection-preview/CollectionPreview.js'
-import { selectCollections } from "../../redux/shop/shopSelectors.js"
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/CollectionsOverview.js"
 
-const Shop = ({collections}) => {
 
-     return (
-     <div className="shop-page">
-          {
-               collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-     ))
-          }
-
-     </div>
-     )
-     
-
-}
-
-const mapStateToProps = createStructuredSelector({
-     collections: selectCollections
-})
+const Shop = ({ collections }) => {
+  return (
+    <div className="shop-page">
+       <CollectionsOverview />
+      )
+    </div>
+  );
+};
 
 
 
-export default connect(mapStateToProps)(Shop)
+export default Shop;
