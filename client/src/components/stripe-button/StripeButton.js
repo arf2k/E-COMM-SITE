6e13eaa69374c0ebf48290f99c62a87aa664d055
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-02-27 17:35:00 -0500

    Front and backend linked but error needs to be fixed

--- client/src/components/stripe-button/StripeButton.js
+++ client/src/components/stripe-button/StripeButton.js
@@ -1,27 +1,46 @@
 import React from "react";
-import StripeCheckout from 'react-stripe-checkout';
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { response } from "express";
 
 const StripeButton = ({ price }) => {
- const stripePrice = price * 100
- const publishableKey = "pk_test_51IMIyLAVI7YMiFvEzuSzpzIpqCEe8VX93X49Nk8AnmVaHeSoePJQqcJBWXBeph0spqOUoVg1vf1sUwIZv04MPMP900L5XVcGC8"
+  const stripePrice = price * 100;
+  const publishableKey =
+    "pk_test_51IMIyLAVI7YMiFvEzuSzpzIpqCEe8VX93X49Nk8AnmVaHeSoePJQqcJBWXBeph0spqOUoVg1vf1sUwIZv04MPMP900L5XVcGC8";
 
-const onToken = token => {
-    console.log(token)
-     alert("Payment Complete")
-}
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: stripePrice,
+        token: token 
+      }
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", +JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please use provided credit card"
+        );
+      });
+  };
 
- return (
-     <StripeCheckout 
-     label="Buy Now"
-     name="Foreman Ecomm"
-     billingAddress
-     shippingAddress
-     description={`Your total is $${price}`}
-     amount={stripePrice}
-     panelLabel="Buy Now"
-     token={onToken}
-     stripeKey={publishableKey} />
- )
-}
+  return (
+    <StripeCheckout
+      label="Buy Now"
+      name="Foreman Ecomm"
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={stripePrice}
+      panelLabel="Buy Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
 
-export default StripeButton
+export default StripeButton;
